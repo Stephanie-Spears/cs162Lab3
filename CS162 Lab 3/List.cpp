66903eb621b21539454ecdb08a73740935594c30
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-18 17:22:31 -0800

    upload

---
+++ CS162 Lab 3/List.cpp
@@ -0,0 +1,50 @@
+#include "list.h"
+#include <iostream>
+
+using namespace std;
+
+bool insert(int val, int intList[], int& size)
+{
+	intList[size] = val;
+	
+	for (int i = 0; i < size; i++)
+	{
+		if (val == intList[i])
+		{
+			return false;
+		}
+		else if (val < intList[i])
+		{
+			intList[i + 1] = intList[i];
+			intList[i] = val;
+		}
+	}
+	size = size + 1;
+
+	return true;
+}
+
+bool remove(int val, int intList[], int& size)
+{
+	for (int i = 0; i < size +1; i++)
+	{
+		if (val == intList[i])
+		{
+			intList[i] = intList[i + 1];
+			size--;
+			return true;
+		}
+	}
+	return false; 
+}
+
+void print(const int intList[], int size)
+{
+	cout << endl << "[ ";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << intList[i] << " ";
+	}
+	cout << "]" << endl;
+}
---
+++ CS162 Lab 3/List.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef LIST_H
+#define LIST_H
+
+bool insert(int val, int intList[], int& size);
+bool remove(int val, int intList[], int& size);
+void print(const int intList[], int size);
+
+#endif
